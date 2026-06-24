c37c85118c21f73f8bc8b8e547a74abbb4049329
YourFutureHome Developer <developer@yourfuturehome.com>
2026-06-25 00:57:29 +0530

    Add root GET route handler to prevent Cannot GET / error

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send("YourFutureHome Backend API is running successfully!");
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
